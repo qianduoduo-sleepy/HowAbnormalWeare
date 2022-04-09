e9518a1a4c34f90466cb2619172fdcc80b16e081
qianduoduo-sleepy <2865179233@qq.com>
2022-04-09 21:42:59 +0800

    fix cake

--- js/effect.js
+++ js/effect.js
@@ -215,7 +215,7 @@ $('document').ready(function(){
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
+			if(i==53){
 				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
 				});
