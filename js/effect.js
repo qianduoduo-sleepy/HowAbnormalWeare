5af9d4c85c00acfc1c039218b372a6b6a6f33b4e
pllj <1169614863@qq.com>
2022-04-10 19:53:05 +0800

    Captailize

--- js/effect.js
+++ js/effect.js
@@ -215,8 +215,8 @@ $('document').ready(function(){
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==53){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+			if(i==52){
+				$("p:nth-child(52)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
 				});
 				
